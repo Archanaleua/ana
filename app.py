70932a38f75e8b0ff86939f4b2ba6c1b7d9233e6
Archana Leua <archanaleua09@gmail.com>
2026-06-27 15:43:51 +0530

    fix gunicorn app factory

--- app.py
+++ app.py
@@ -105,8 +105,9 @@ def create_app() -> Flask:
 
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(
         host="0.0.0.0",
         port=int(os.getenv("PORT", "5000")),
